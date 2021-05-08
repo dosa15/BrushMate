766f7bfca6d8cf3131a57711798bd7743cef666a
Dhushyanth MK <dhushyanthmohan.kumar2019@vitstudent.ac.in>
2021-05-09 03:33:37 +0530

    Added functionality to draw line; doesn't work

--- main.py
+++ main.py
@@ -1,43 +1,95 @@
 from BrushMateTM import *
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtGui import QPainter
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 import sys, random
 
 
+class GraphicsScene(QGraphicsScene):
+    
+    def __init__(self, parent=None):
+        QGraphicsScene.__init__(self, parent)
+        self.setSceneRect(0, 0, 660, 680)
+        self.pen = QPen(Qt.black)
+        self.firstClick = True
+        self.start = self.end = QPointF(0, 0)
+
+    def mousePressEvent(self, event):
+        if self.firstClick:
+            self.start = event.pos()
+            print("Start:", self.start)
+            self.firstClick = False
+        else:
+            self.end = event.pos()
+            print("End:", self.end)
+            drawSomething = QGraphicsLineItem(QLineF(self.start, self.end))
+            drawSomething.setPen(self.pen)
+            print(drawSomething)
+            self.addItem(drawSomething)
+            self.firstClick = True
+
+    # def mouseMoveEvent(self, event):
+    #     self.cursorCurrentPosition = event.scenePos()
+    #     current = QtCore.QPointF(self.cursorCurrentPosition.x(),self.cursorCurrentPosition.y())
+    #     link = QtGui.QGraphicsLineItem(QtCore.QLineF(self.start, current))
+    #     link.setPen(self.pen)
+    #     self.scene.addItem(link)
+
+
 class BrushMateWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+
     def __init__(self, parent=None):
         QtWidgets.QMainWindow.__init__(self, parent=parent)
         self.setupUi(self)
-        self.scene = QtWidgets.QGraphicsScene()
+        self.scene = GraphicsScene(self)
         self.graphicsView.setScene(self.scene)
-        brush = QtGui.QBrush(QtCore.Qt.white)
-        self.scene.setBackgroundBrush(QtBrush=brush, QColor=QtCore.Qt.white)
-        # self.pen = QtGui.QPen(QtCore.Qt.green)
-        # self.scene.addRect(QtCore.QRectF(0, 0, 100, 200), QtGui.QPen(QtCore.Qt.blue), QtGui.QBrush(QtCore.Qt.green))
-    
-    # def paintEvent(self, e):
-    #     qp = QPainter()
-    #     qp.begin(self)
-    #     self.drawPoints(qp)
-    #     qp.end()
+        self.graphicsView.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+        
+    '''
+    def __init__(self, parent=None):
+        QtWidgets.QMainWindow.__init__(self, parent=parent)
+        self.setupUi(self)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.white)
+        self.setAutoFillBackground(True)
+        self.setPalette(p)
+        self.myPixmap = QPixmap(200,200)
+        self.setMinimumSize(200,200)
+        self.painter = QPainter(self.myPixmap)
+        self.pen = QPen(Qt.black)
+        self.painter.setPen(self.pen)
+        self.painter.fillRect(0,0,200,200, Qt.white)
+        self.graphicsView.setPixmap(self.myPixmap)
+        self.last = None
 
-    # def drawPoints(self, qp):
-    #     qp.setPen(Qt.red)
-    #     size = self.size()
+    def mouseMoveEvent(self, event):
+        if self.last:
+            self.painter.drawLine(self.last, event.pos())
+            self.last = event.pos()
+            self.graphicsView.setPixmap(self.myPixmap)
+            self.update()
 
-    #     if size.height() <= 1 or size.height() <= 1:
-    #         return
+    def mousePressEvent(self, event):
+        self.last = event.pos()
 
-    #     for i in range(1000):
-    #         x = random.randint(1, size.width() - 1)
-    #         y = random.randint(1, size.height() - 1)
-    def paintEvent(self, event):
-        painter = QPainter()
-        painter.setPen(QtGui.QPen(Qt.green,  8, Qt.DashLine))
-        painter.drawEllipse(40, 40, 400, 400)
-        # return super().paintEvent(self, QPaintEvent)
-    
+    def mouseReleaseEvent(self, event):
+        self.last = None
+
+    def updateSize(self, width, height):
+        pm = QPixmap(width, height)
+        pm.fill(Qt.white)
+        old = self.myPixmap
+        self.myPixmap = pm
+        self.pen = QPen(Qt.black)
+        self.painter = QPainter(pm)
+        self.painter.drawPixmap(0,0,old)
+        self.graphicsView.setPixmap(pm)
+
+    def resizeEvent(self, event):
+        if event.oldSize().width() > 0:
+            self.updateSize(event.size().width(), event.size().height())
+    '''
 
 
 if __name__ == '__main__':
@@ -45,15 +97,5 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     w = BrushMateWindow()
     w.show()
-    sys.exit(app.exec_())
-
-
-if __name__ == "__main_1_":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
     sys.exit(app.exec_())
 
