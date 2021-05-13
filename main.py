0a3e7a3477def137dc2c00d85f62e993fe7d850b
Clivin <clivin100@gmail.com>
2021-05-13 11:16:21 +0400

    some other changes to circle and square

--- main.py
+++ main.py
@@ -77,6 +77,12 @@ class GraphicsScene(QGraphicsScene):
                     self.start.x,self.end.x=self.end.x,self.start.x
                 if(self.start.y() > self.end.y()):
                     self.start.y,self.end.y=self.end.y,self.start.y
+                if(self.end.x() - self.start.x() > self.end.y() - self.start.y()):
+                    self.start.x = self.start.y
+                    self.end.x = self.end.y
+                if(self.end.x() - self.start.x() < self.end.y() - self.start.y()):
+                    self.start.y = self.start.x
+                    self.end.y = self.end.x
                 if(self.end.x() - self.start.x() == self.end.y() - self.start.y()):
                     self.addRect(QRectF(QPointF(self.start.x(), self.start.y()),QPointF(self.end.x(), self.end.y())))
                 self.firstClickSquare = True
@@ -91,6 +97,12 @@ class GraphicsScene(QGraphicsScene):
                     self.start.x,self.end.x=self.end.x,self.start.x
                 if(self.start.y() > self.end.y()):
                     self.start.y,self.end.y=self.end.y,self.start.y
+                if(self.end.x() - self.start.x() > self.end.y() - self.start.y()):
+                    self.start.x = self.start.y
+                    self.end.x = self.end.y
+                if(self.end.x() - self.start.x() < self.end.y() - self.start.y()):
+                    self.start.y = self.start.x
+                    self.end.y = self.end.x
                 if(self.end.x() - self.start.x() == self.end.y() - self.start.y()):
                     self.addEllipse(QRectF(QPointF(self.start.x(), self.start.y()),QPointF(self.end.x(), self.end.y())))
                 self.firstClickCircle = True
