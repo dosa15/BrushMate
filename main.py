a9bc1a6ee9dc168988249366b90e8c1615648a4c
Dhushyanth MK <dhushyanthmohan.kumar2019@vitstudent.ac.in>
2021-05-14 22:56:59 +0530

    Fixed indentation errors

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ class AnotherWindow(QWidget):
     def __init__(self):
         super().__init__()
         layout = QVBoxLayout()
-        self.label = QLabel("Another Window % d" % randint(0,100))
+        self.label = QLabel("Another Window %d" % random.randint(0,100))
         layout.addWidget(self.label)
         self.setLayout(layout)
 
@@ -200,48 +200,48 @@ class BrushMateWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     class CommandAdd(QUndoCommand):
 
-    def __init__(self, listWidget, row, string, description):
-        super(CommandAdd, self).__init__(description)
-        self.listWidget = listWidget
-        self.row = row
-        self.string = string
+        def __init__(self, listWidget, row, string, description):
+            super(CommandAdd, self).__init__(description)
+            self.listWidget = listWidget
+            self.row = row
+            self.string = string
 
-    def redo(self):
-        self.listWidget.insertItem(self.row, self.string)
-        self.listWidget.setCurrentRow(self.row)
+        def redo(self):
+            self.listWidget.insertItem(self.row, self.string)
+            self.listWidget.setCurrentRow(self.row)
 
-    def undo(self):
-        item = self.listWidget.takeItem(self.row)
-        del item
+        def undo(self):
+            item = self.listWidget.takeItem(self.row)
+            del item
 
-    def delete(self):
-        row = self.listWidget.currentRow()
-        item = self.listWidget.item(row)
-        if item is None:
-            return
-        reply = QMessageBox.question(self, "Remove %s" % self.name,
-                        "Remove %s ´%s’?" % (
-                        self.name, unicode(item.text())),
-                        QMessageBox.Yes|QMessageBox.No)
-        if reply == QMessageBox.Yes:
-            command = CommandDelete(self.listWidget, item, row,
-                                    "Delete (%s)" % item.text())
-            self.undoStack.push(command)
+        def delete(self):
+            row = self.listWidget.currentRow()
+            item = self.listWidget.item(row)
+            if item is None:
+                return
+            reply = QMessageBox.question(self, "Remove %s" % self.name,
+                            "Remove %s ´%s’?" % (
+                            self.name, unicode(item.text())),
+                            QMessageBox.Yes|QMessageBox.No)
+            if reply == QMessageBox.Yes:
+                command = CommandDelete(self.listWidget, item, row,
+                                        "Delete (%s)" % item.text())
+                self.undoStack.push(command)
             
     class CommandDelete(QUndoCommand):
 
-    def __init__(self, listWidget, item, row, description):
-        super(CommandDelete, self).__init__(description)
-        self.listWidget = listWidget
-        self.string = item.text()
-        self.row = row
+        def __init__(self, listWidget, item, row, description):
+            super(CommandDelete, self).__init__(description)
+            self.listWidget = listWidget
+            self.string = item.text()
+            self.row = row
 
-    def redo(self):
-        item = self.listWidget.takeItem(self.row)
-        del item
+        def redo(self):
+            item = self.listWidget.takeItem(self.row)
+            del item
 
-    def undo(self):
-        self.listWidget.insertItem(self.row, self.string)
+        def undo(self):
+            self.listWidget.insertItem(self.row, self.string)
 
     def show_new_window(self, checked):
         if self.w is None:
