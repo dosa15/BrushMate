132832db9c3b5b8a84ea4ac45a9912c27cafca81
Clivin <clivin100@gmail.com>
2021-05-14 16:04:50 +0400

    added "new window" feature

--- main.py
+++ main.py
@@ -144,6 +144,14 @@ class GraphicsScene(QGraphicsScene):
         if eraserDraw:
             eraserDraw = False
 
+class AnotherWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.label = QLabel("Another Window % d" % randint(0,100))
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
 
 class BrushMateWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
@@ -173,6 +181,19 @@ class BrushMateWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.filename = None
         self.actionSave.triggered.connect(self.fileSave)
         self.actionSave_As.triggered.connect(lambda: self.fileSave(saveAs=True))
+
+        self.w = None  # No external window yet.
+        self.button = QPushButton("Push for Window")
+        self.button.clicked.connect(self.show_new_window)
+        self.setCentralWidget(self.button)
+
+    def show_new_window(self, checked):
+        if self.w is None:
+            self.w = AnotherWindow()
+            self.w.show()
+        else:
+            self.w.close()  # Close window.
+            self.w = None  # Discard reference.
 
     def retranslateUi(self, MainWindow):
         _translate = QCoreApplication.translate
